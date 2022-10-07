3e0d6cb956ff4266e0d10aada8ae30c6d46cf1d5
EZEQUIEL GUZMAN <57122751+ezequiel-guzman@users.noreply.github.com>
2022-10-07 07:02:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+console.log(array);
+
+// .splice
+
+array.splice(0,1);
+
+console.log(array);
+
+// .sort
+
+array.sort();
+
+console.log(array);
+
+// .push
+
+array.push("kiwi");
+
+console.log(array);
+
+// .splice
+
+array.splice(0,1);
+
+console.log(array);
+
+//  .reverse 
+
+array.reverse();
+
+console.log(array);
+
+// access oranges from this array
+
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(array2[1][1])
